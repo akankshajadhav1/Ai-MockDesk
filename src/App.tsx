866fb5ea03b38ca5a1c9979af54020926e512cbc
akankshajadhav1 <75427676+akankshajadhav1@users.noreply.github.com>
2025-06-09 18:58:43 +0530

    Changes: clerk authentication and routing  setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { Button } from "./components/ui/button"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { PublicLayout } from "./layouts/public-layout"
+import Homepage from "./routes/home"
+import AuthenticationLayout from "./layouts/auth-layout"
 
-
+import SignInPage from "./routes/signIn"
+import SignUpPage from "./routes/signUp"
+import ProtectedRoutes from "./layouts/protected-routes"
+import { MainLayout } from "./layouts/main-layout"
 const App = () => {
   return (
-    <div className=' text-3xl text-blue-400 font-thin'>
-      <Button variant={"outline"}>Click Me</Button>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<PublicLayout/>}>
+        <Route index element={<Homepage/>}/>
+        </Route>
+        {/* Authentication layout */}
+        <Route element={<AuthenticationLayout/>}>
+        <Route path="/signin/*" element={<SignInPage/>}/>
+        <Route path="/signup/*" element={<SignUpPage/>}/>
+        </Route>
+        
+        {/* Protected Route */}
+       <Route element={<ProtectedRoutes><MainLayout/></ProtectedRoutes>}>
+        {/*  add All protected routes */}
+             
+       </Route>
+      </Routes>
+    </Router>
   )
 }
 
