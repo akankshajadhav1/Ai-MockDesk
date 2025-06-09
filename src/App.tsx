73f027bc039ce8dd9230a19440f1674834bd9c36
akankshajadhav1 <75427676+akankshajadhav1@users.noreply.github.com>
2025-06-09 16:11:55 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+
+const App = () => {
+  return (
+    <div className=' text-3xl text-blue-400 font-thin'>
+      <Button variant={"outline"}>Clickme</Button>
+    </div>
+  )
+}
+
+export default App
